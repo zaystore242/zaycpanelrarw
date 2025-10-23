9d417800a4a67b43c25b0df19184bac58228815e
zaystore242 <yudaaguz099@gmail.com>
2025-10-23 18:26:37 +0800

    Update login.js

--- login/login.js
+++ login/login.js
@@ -1,15 +1,16 @@
 module.exports = {
   users: [
     { username: "tama", password: "123" },
-    { username: "1", password: "1" }
     { username: "1", password: "1" },
-    { username: "1", password: "1" }
     { username: "1", password: "1" },
-    { username: "1", password: "1" }
     { username: "1", password: "1" },
-    { username: "1", password: "1" }
+    { username: "1", password: "1" },
+    { username: "1", password: "1" },
+    { username: "1", password: "1" },
+    { username: "1", password: "1" },
   ],
   admins: [
     { username: "Zaystore", password: "241010" },
   ]
+
 };
