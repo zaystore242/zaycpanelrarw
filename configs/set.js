2f0f883929f9300bef0ed49726f947b266e912d8
zaystore242 <yudaaguz099@gmail.com>
2025-10-23 17:57:10 +0800

    Add files via upload

---
+++ configs/set.js
@@ -0,0 +1,5 @@
+module.exports = {
+  domain: "https://kayzenxsidzzz.pterodactyl-panel.web.id",
+  apikeyptero: "ptla_gkkgNl5OAbUI3GZJiYDjJjLpmwIdE6hf59M1HkuB5GH",
+  capikeyptero: "ptlc_aVwXye4K4Adtb0ERjW0y11cWESYJ3Q5aTrdVrIyWCpV"
+};
