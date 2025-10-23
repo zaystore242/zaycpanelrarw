84d881189ef212dfbf0fd102343064032d6dfdf0
zaystore242 <yudaaguz099@gmail.com>
2025-10-23 18:24:16 +0800

    Add files via upload

---
+++ login/login.js
@@ -0,0 +1,15 @@
+module.exports = {
+  users: [
+    { username: "tama", password: "123" },
+    { username: "1", password: "1" }
+    { username: "1", password: "1" },
+    { username: "1", password: "1" }
+    { username: "1", password: "1" },
+    { username: "1", password: "1" }
+    { username: "1", password: "1" },
+    { username: "1", password: "1" }
+  ],
+  admins: [
+    { username: "Zaystore", password: "241010" },
+  ]
+};
