1d6a7cf153c9057eb36383907f82e89b3ec434a0
zaystore242 <yudaaguz099@gmail.com>
2025-10-25 22:09:15 +0800

    Update set.js

--- configs/set.js
+++ configs/set.js
@@ -1,9 +1,9 @@
 module.exports = {
-  domain: "https://public-server.kingstoree.web.id",
+  domain: "https://public-server.kingstoree.web.id/",
   apikeyptero: "ptla_3AWdNd41JQGm7ZFXR0gaOCWl4sbjQ2vtXKdZXjZS7r8",
   capikeyptero: "ptlc_1wmDkNUZTWcexa1EFfczCZ5heB8S0C9xEBdjTXf9YJF"
-
 };
+
 
 
 
