63ed8ef398af57fe7dee56522dc0dd332a5a9bcf
zaystore242 <yudaaguz099@gmail.com>
2025-10-25 10:56:44 +0800

    Update set.js

--- configs/set.js
+++ configs/set.js
@@ -1,7 +1,8 @@
 module.exports = {
   domain: "https://alluffyhosting.new.server.lightsecretaja.web.id",
   apikeyptero: "ptla_DZSOPWMkeB4K4TGumM6wC0t6NiUGS4FzMn7LVmRDLsG",
-  capikeyptero: "ptlc_iARLbsAXIQ0sVGvR8pBDAPg7qGVXKTL7ww0uorBxd6a"
+  capikeyptero: "ptlc_8blDlX0cO45VOsmKFzRzS69fNPl1o9BUW6g1e5zYDy6"
 
 };
+
 
