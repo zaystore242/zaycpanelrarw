6890411f39f5cdab408c8f54aea491b099f47287
zaystore242 <yudaaguz099@gmail.com>
2025-10-30 13:32:23 +0800

    Update set.js

--- configs/set.js
+++ configs/set.js
@@ -1,8 +1,9 @@
 module.exports = {
-  domain: "https://syahpaneladpprivate.pterodactyl-panel.web.id",
-  apikeyptero: "ptla_B4E8CkrKmRANSDKkoNhRwyLILzKuAbGzpPpEMgb6Tet",
-  capikeyptero: "ptlc_eJzyulkk4i4s31buRdmg3O1wKDLTAN5DCTkR03r6nGD"
+  domain: "https://syahadppanelprivatee.storeid.my.id",
+  apikeyptero: "ptla_Ww79a9hE6QdAopHcgQY4ikajClIs2miX7ZoFfioLyjz",
+  capikeyptero: "ptlc_wl4YXm0womjuZS97Pfidc6l3fIQZU5H4meZaADDNCZF"
 };
+
 
 
 
