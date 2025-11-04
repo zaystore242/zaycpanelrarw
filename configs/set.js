04fc19a8bae7e404484760d13909ad763a919a8e
zaystore242 <yudaaguz099@gmail.com>
2025-11-04 13:35:53 +0800

    Update set.js

--- configs/set.js
+++ configs/set.js
@@ -1,8 +1,9 @@
 module.exports = {
-  domain: "https://syahadppanelprivatee.storeid.my.id",
-  apikeyptero: "ptla_Ww79a9hE6QdAopHcgQY4ikajClIs2miX7ZoFfioLyjz",
-  capikeyptero: "ptlc_wl4YXm0womjuZS97Pfidc6l3fIQZU5H4meZaADDNCZF"
+  domain: "https://syahadppanelpriv.centzzcloud.my.id",
+  apikeyptero: "ptla_AW619EF0COnrYaR3CGjxbntCl36MzDw4KPXO8a9azdv",
+  capikeyptero: "ptlc_HwI19VdIkv2xBT7zkVj2BxRcsdayYTJ9ZcmIfFDClyD"
 };
+
 
 
 
