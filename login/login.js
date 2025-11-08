e8c595a8a82307136911ec895a108bcafb60efcb
zaystore242 <yudaaguz099@gmail.com>
2025-11-08 20:22:51 +0800

    Update login.js

--- login/login.js
+++ login/login.js
@@ -1,20 +1,13 @@
 module.exports = {
   users: [
-    { username: "tama", password: "123" },
-    { username: "Yuzitiw", password: "Yuzitiw23" },
-    { username: "Cris", password: "Cris12" },
-    { username: "kyoo", password: "2709" },
-    { username: "Dikz", password: "tama" },
-    
+    { username: "tama", password: "123" },  
   ],
   admins: [
     { username: "Zaystore", password: "241010" },
-    { username: "Guardians", password: "Guardians001" },
-    { username: "Tom", password: "Tom" },
-    { username: "ceon", password: "koshikaito" },
   ]
 
 };
+
 
 
 
